a63dfe9e43c282b8b91f59466d773050177ea27a
Allen Guan <me@nella17.tw>
2023-03-19 04:00:15 +0800

    [Lab1] add verifier

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eux
 CXX=g++-12 make -j
+mkdir -p output verify
 fd . ./testbench -x bash -c '
 ./Lab1 {} ./output/{/} 2>/dev/null &&
 python ./plotter.py \
   --in_file {} \
   --out_file ./output/{/} \
-  --img_name ./output/{/.}.png
+  --img_name ./output/{/.}.png &&
+./verifier ./output/{/} {} | tee ./verify/{/}
 '
