0b5ec81165fbedf089f536c867d07bf1227d5ed2
Allen Guan <me@nella17.tw>
2023-05-07 02:09:25 +0800

    [Lab2] update test.sh default

--- Lab2/test.sh
+++ Lab2/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 make -j
-in=${1-'ISPD 2008 Benchmarks/adaptec1.gr'}
+in=${1-'ISPD 2008 Benchmarks/newblue2.gr'}
 out=${in}.out
 /usr/bin/time -lp ./router "$in" "$out"
 time perl ./eval2008.pl "$in" "$out"
