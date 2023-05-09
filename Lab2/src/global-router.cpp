f3b875d79b2a03009f4c8fe08c46235a1f78e448
Allen Guan <me@nella17.tw>
2023-05-09 17:09:08 +0800

    [Lab2] try simple cost

--- Lab2/src/global-router.cpp
+++ Lab2/src/global-router.cpp
@@ -133,8 +133,11 @@ ld GlobalRouter::cost(ISPDParser::Net* net, const Edge& e) const {
     if (net and e.net.count(net->id)) return 1;
 
     // return std::exp(std::max(1, e.demand - e.cap + 1) * 2);
-    auto used = !net ? 0 : std::max(net->minimumWidth, min_width) + min_spacing;
-    auto of = e.cap - (e.demand + used);
+    auto demand = e.demand + (!net ? 0 : std::max(net->minimumWidth, min_width) + min_spacing);
+    auto of = e.cap - demand;
+
+    auto r = of < 0 ? 1e5 : 0.1;
+    return 1 + r * demand / (1.0 + e.cap);
     auto dah = pow(e.he, 1.5) / (7 + 4 * k);
     auto pe = 1 + 150 / (1 + std::exp(0.3 * of));
     auto be = 10 + 100 / std::pow(2, k);
