076b5ef31c4189865ba8db357b7cdd92423d654f
Allen Guan <me@nella17.tw>
2023-05-24 10:43:09 +0800

    [Lab2] testall-parallel.sh

--- Lab2/testall-parallel.sh
+++ Lab2/testall-parallel.sh
@@ -4,18 +4,6 @@ tl=${1-$((30 * 60))}
 
 function prepend() { while read -r line; do echo "${1}${line}"; done; }
 
-function check() {
-  in=$1
-  out=$1.all.out
-  time=$1.txt
-  case=$(basename "$in" | cut -d'.' -f1)
-  rt=$(tail "$time" | grep real | awk '{ print $2 }')
-  time perl ./eval2008.pl "$in" "$out" 2>&1 | tee -a "$time" | \
-    tail -n1 | tail "-c+$(echo "$in x" | wc -c | awk '{ print $1 }')" | \
-    awk "{ print \"$case\" \"\t\" \$1 \"\t\" \$2 \"\t\" \$3 \"\t\" \"$rt\" }" | tee -a "$result"
-  rm "$out"
-}
-
 route() {
   in=$1
   out=$1.all.out
@@ -23,7 +11,12 @@ route() {
   timeout "$tl" /usr/bin/time -p ./router "$in" "$out" "$tl" 2>&1 | tee "$time" | prepend "$(basename "$in") "
   if [ $? ]; then
     echo "$1 done"
-    check "$1"
+    case=$(basename "$in" | cut -d'.' -f1)
+    rt=$(tail "$time" | grep real | awk '{ print $2 }')
+    time perl ./eval2008.pl "$in" "$out" 2>&1 | tee -a "$time" | \
+      tail -n1 | tail "-c+$(echo "$in x" | wc -c)" | \
+      awk "{ print \"$case\" \"\t\" \$1 \"\t\" \$2 \"\t\" \$3 \"\t\" \"$rt\" }" | tee -a "$result"
+    rm "$out"
   else
     echo "route fail"
   fi
@@ -33,6 +26,6 @@ make -j || exit
 echo -e "case\tTOF\tMOF\tWL\tRT" > "$result"
 for f in './ISPD 2008 Benchmarks'/*.gr; do
   echo "$f"
-  route "$f"
+  route "$f" &
 done
 wait
