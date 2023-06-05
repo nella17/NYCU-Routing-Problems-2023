79305f447f17ae27ee40b66ad9d74475be79ff03
Allen Guan <me@nella17.tw>
2023-06-05 11:49:13 +0800

    [Lab2] fix test.sh

--- Lab2/test.sh
+++ Lab2/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 make -j
-in=${1-'ISPD-2008-Benchmarks/newblue2.gr'}
+in=${1-'ISPD-2008-Benchmarks/newblue2.fastplace90.3d.50.20.100.gr'}
 out=${in}.out
 t=${2-600}
 /usr/bin/time -p ./router "$in" "$out" "$t"
